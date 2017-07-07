cdf33648f6e90f8abb7bb9c3b5fdd2e62c57ecf6
DESKTOP-N2KHJ6D\vietduc <vd>
2017-07-07 13:47:37 +0700

    no message

---
+++ http-elearning-web/src/main/java/rest/ApplicationConfig.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rest;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author vietduc
+ */
+@javax.ws.rs.ApplicationPath("webresources")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(rest.PermissionFacadeREST.class);
+        resources.add(rest.RoleFacadeREST.class);
+        resources.add(rest.RolepermissionFacadeREST.class);
+        resources.add(rest.UserFacadeREST.class);
+    }
+    
+}
